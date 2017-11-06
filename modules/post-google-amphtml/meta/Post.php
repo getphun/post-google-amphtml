4df03b79d05b33d569293061d9b6c288de843727
Iqbal Fauzi <iqbalfawz@gmail.com>
2017-11-06 13:57:38 +0700

    More post meta

--- modules/post-google-amphtml/meta/Post.php
+++ modules/post-google-amphtml/meta/Post.php
@@ -18,28 +18,56 @@ class Post
         $meta_desc  = $post->meta_description->safe;
         if(!$meta_desc && $post->content)
             $meta_desc = $post->content->chars(160);
-        $meta_image = $post->cover;
-        if(!$meta_image)
+        
+        if(!$post->cover)
             $meta_image = $base_url . 'theme/site/static/logo/500x500.png';
+        else
+            $meta_image = $post->cover->abs;
+        
         $meta_url   = $post->page;
         $meta_title = $post->meta_title;
         $meta_keys  = $post->meta_keywords;
-        if(!$meta_title == '')
+        if($meta_title == '')
             $meta_title = $post->title;
         
         // metas
         $single = (object)[
             'isamp'    => true,
             '_schemas' => [],
-            '_metas'   => [
-                'title'         => $meta_title,
-                'canonical'     => $meta_url,
-                'description'   => $meta_desc,
-                'keywords'      => $meta_keys,
-                'image'         => $meta_image,
-                'type'          => 'website'
-            ]
+            '_metas'   => []
         ];
+        
+        $metas = [
+            'title'         => $meta_title,
+            'canonical'     => $meta_url,
+            'description'   => $meta_desc,
+            'keywords'      => $meta_keys,
+            'image'         => $meta_image,
+            'type'          => 'article',
+            'updated_time'  => $post->updated->format('c'),
+            'published_time'=> $post->published->format('c'),
+            
+            // TODO
+            // use fb profile url instead
+            'article:author'=> $post->user->fullname
+        ];
+        if($dis->setting->social_facebook)
+            $metas['article:publisher'] = $dis->setting->social_facebook;
+        
+        if(isset($post->tag) && $post->tag){    
+            $tags = [];
+            foreach($post->tag as $tag)
+                $tags[] = $tag->name;
+            if($tags)
+                $metas['article:tag'] = $tags;
+        }
+        
+        if(is_object($post->canal))
+            $metas['article:section'] = $post->canal->name;
+        elseif(isset($post->category) && $post->category)
+            $metas['article:section'] = $post->category[0]->name;
+        
+        $single->_metas = $metas;
         
         // schemas 
         if(!$post->schema_type)
@@ -50,14 +78,30 @@ class Post
             '@type'         => $post->schema_type,
             'name'          => $post->title,
             'description'   => $meta_desc,
+            'url'           => $meta_url,
+            'image'         => $meta_image,
+            'headline'      => substr($meta_desc, 0, 110),
+            'datePublished' => $post->published->format('c'),
+            'dateModified'  => $post->published->format('c'),
+            'author'        => [
+                '@type'         => 'Person',
+                'name'          => $post->user->fullname
+            ],
+            'mainEntityOfPage' => [
+                '@type'             => 'WebPage',
+                '@id'               => $meta_url
+            ],
             'publisher'     => [
                 '@type'         => 'Organization',
                 'name'          => $dis->config->name,
                 'url'           => $base_url,
-                'logo'          => $base_url . 'theme/site/static/logo/500x500.png'
-            ],
-            'url'           => $meta_url,
-            'image'         => $meta_image
+                'logo'          => [
+                    '@type'         => 'ImageObject',
+                    'url'           => $base_url . 'theme/site/static/logo/200x60.png',
+                    'height'        => 60,
+                    'width'         => 200
+                ]
+            ]
         ];
         $single->_schemas[] = $schema;
         
